37dadf72cc7768f09b9d2fb33f5d25e9d0e24023
pusoxx <135089815+pusoxx@users.noreply.github.com>
2023-06-13 22:02:51 +0700

    Add files via upload

---
+++ instal-udp.sh
@@ -0,0 +1,5 @@
+echo -e " [INFO] Sabar sayang Lagi Proses"
+wget https://raw.githubusercontent.com/pusoxx/Bless/main/Tunnel/udp.sh && bash udp.sh
+echo -e " [INFO] UDP Sukses Di Pasang"
+sleep 2
+menu
