c5d38d274bf3450ac7819733f8fc4b343d1265ab
pusoxx <135089815+pusoxx@users.noreply.github.com>
2023-06-13 22:41:28 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -255,9 +255,11 @@ echo -e "${YELLOW}━━━━━━━━━━━━━━━━━━━━�
 
 echo -e " ${BIRed}❐ SYSTEM OS  ${NC}          : ${green}$( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' ) ( $( uname -m) )${NC}"
 echo -e " ${BIRed}❐ SERVER RAM  ${NC}         : ${green}${totalram}MB"
+echo -e " ${BIRed}❐ RAM USAGE  ${NC}          : ${green}$USAGERAM MB${NC}"
 echo -e " ${BIRed}❐ SERVER UPTIME ${NC}       : ${green}$( uptime -p  | cut -d " " -f 2-10000 ) ${NC}"
-echo -e " ${BIRed}❐ DATE & TIME ${NC}         :  ${green}$( date -d "0 days" +"%d-%m-%Y | %X" )${NC}"
+echo -e " ${BIRed}❐ DATE & TIME ${NC}         : ${green}$( date -d "0 days" +"%d-%m-%Y | %X" )${NC}"
 echo -e " ${BIRed}❐ DOMAIN ${NC}              : ${green}$( cat /etc/xray/domain )${NC}"
+echo -e " ${BIRed}❐ CITY ${NC}                : ${green}$CITY${NC}"
 echo -e " ${BIRed}❐ IP VPS ${NC}              : ${green}$IPVPS${NC}"
 echo -e " ${BIRed}❐ ISP VPS ${NC}             : ${green}$ISP${NC}"
 echo -e " ${BIRed}❐ CLIENTS NAME ${NC}        : ${green}$Name${NC}"
