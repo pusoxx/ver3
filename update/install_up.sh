c57b5cbcc5f5cfc00543361a8083fb48a7a82d66
pusoxx <135089815+pusoxx@users.noreply.github.com>
2023-06-21 19:07:54 +0700

    Add files via upload

--- update/install_up.sh
+++ update/install_up.sh
@@ -36,6 +36,7 @@ wget -q -O /usr/bin/del-ws "https://raw.githubusercontent.com/artanodrop/v4/main
 wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/xp.sh" && chmod +x /usr/bin/xp
 wget -q -O /usr/bin/autokill "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/autokill.sh" && chmod +x /usr/bin/autokill
 wget -q -O /usr/bin/add-host "https://raw.githubusercontent.com/pusoxx/ver3/main/ssh/add-host.sh" && chmod +x /usr/bin/add-host
+wget -q -O /usr/bin/tcp "https://raw.githubusercontent.com/pusoxx/ver3/main/tcp.sh" && chmod +x /usr/bin/tcp
 wget https://raw.githubusercontent.com/pusoxx/ver3/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh
 echo -e " [INFO] Update Successfully"
 sleep 2
