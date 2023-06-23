406bdb05380122e63b69de8dc188661946bbd036
pusoxx <135089815+pusoxx@users.noreply.github.com>
2023-06-23 07:17:06 +0700

    Add files via upload

--- up.sh
+++ up.sh
@@ -62,9 +62,9 @@ wget -O genssl "https://raw.githubusercontent.com/pusoxx/ver3/main/ssh/genssl.sh
 wget -O menu-trial "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-trial.sh"
 wget -O menu-vmess "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-vmess.sh"
 wget -O menu-vless "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-vless.sh"
-wget -O running "https://raw.githubusercontent.com/artanodrop/v4/main/menu/running.sh"
+wget -O running "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/running.sh"
 wget -O clearcache "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/clearcache.sh"
-wget -O menu-trgo "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-trgo.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-trgo.sh"
 wget -O menu-trojan "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-trojan.sh"
 wget -O cf "https://raw.githubusercontent.com/pusoxx/ver3/main/cf.sh"
 wget -O slow "https://raw.githubusercontent.com/pusoxx/ver3/main/slow.sh"
@@ -87,14 +87,14 @@ wget -O tendang "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/tendan
 
 # menu system
 wget -O menu-set "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-set.sh"
-wget -O menu-domain "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-domain.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-domain.sh"
 wget -O add-host "https://raw.githubusercontent.com/pusoxx/ver3/main/ssh/add-host.sh"
 wget -O port-change "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/artanodrop/v4/main/xray/certv2ray.sh"
-wget -O menu-webmin "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-webmin.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/menu-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/speedtest_cli.py"
 wget -O about "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/artanodrop/v4/main/menu/auto-reboot.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/pusoxx/ver3/main/menu/auto-reboot.sh"
 wget -O restart "https://raw.githubusercontent.com/artanodrop/v4/main/menu/restart.sh"
 wget -O bw "https://raw.githubusercontent.com/artanodrop/v4/main/menu/bw.sh"
 
@@ -106,7 +106,7 @@ wget -O port-ovpn "https://raw.githubusercontent.com/artanodrop/v4/main/port/por
 wget -O xp "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/xp.sh"
 wget -O acs-set "https://raw.githubusercontent.com/artanodrop/v4/main/acs-set.sh"
 
-wget -O sshws "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/sshws.sh"
+wget -O sshws "https://raw.githubusercontent.com/pusoxx/ver3/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/artanodrop/v4/main/status.sh"
 wget -O menu-backup "https://raw.githubusercontent.com/pusoxx/ver3/main/backup/menu-backup.sh"
 wget -O backup "https://raw.githubusercontent.com/pusoxx/ver3/main/backup/backup.sh"
